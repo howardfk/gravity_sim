2a0213aafc8c39b4c0c81ea4518fdc7c69d07658
howard <howardfk@gmail.com>
2015-03-09 17:38:44 -0400

    adding a flock_sim.py to simulate flock movement, only basic class outline

---
+++ flock_sim.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+class Flock(object):
+   """Takes in an array of birds"""
+   def __init__(self, birds):
+
+   def get_com(self):
+      self.pos
+      return com
+
+   def update(self, birds):
+
+class Bird(object):
+   def __init__(self, pos=np.array([0,0,0], dtype=float32), vel = np.array([0,0,0], dtype=float32) ):
+      self.pos = pos
+      self.vel = vel
+
+   def get_pos(self):
+      return self.pos
+
+   def update(self, target):
+#feed target update velcoty in direction of target
+
+
+#agent based modling 
