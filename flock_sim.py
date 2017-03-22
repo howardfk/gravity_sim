d4a01505a36facf0433a8fa82c940e524540e839
Howard Kim <howardfk@gmail.com>
2017-03-22 13:55:47 -0500

    Add update method for flock positioning

--- flock_sim.py
+++ flock_sim.py
@@ -3,12 +3,16 @@
 class Flock(object):
    """Takes in an array of birds"""
    def __init__(self, birds):
+      self.birds=birds
 
    def get_com(self):
       self.pos
       return com
 
-   def update(self, birds):
+   def update(self):
+      for bird in self.birds:
+         bird.update(target)
+      
 
 class Bird(object):
    def __init__(self, pos=np.array([0,0,0], dtype=float32), vel = np.array([0,0,0], dtype=float32) ):
